1de1917e4af315e60ce3b447d5bc0cad5b271d08
jusx <hello@justinlaw.org>
2019-02-28 01:03:47 -0800

    test: remove redundant test.

--- __tests__/object-dot.js
+++ __tests__/object-dot.js
@@ -1,21 +1,21 @@
-const odotty = require('object-dot')
+const odjectd = require('object-dot')
 
 describe('set', ()=> {
   test('deep nested', ()=> {
     let object = {}
-    odotty.set({ object, path: 'a.b.c.d.e.f.g.h.i.j.k', value: 'my value' })
+    odjectd.set({ object, path: 'a.b.c.d.e.f.g.h.i.j.k', value: 'my value' })
     expect(object.a.b.c.d.e.f.g.h.i.j.k).toBe('my value')
   })
 
   test('medium nested', ()=> {
     let object = {}
-    odotty.set({ object, path: 'a.b.c.d', value: 'my value' })
+    odjectd.set({ object, path: 'a.b.c.d', value: 'my value' })
     expect(object.a.b.c.d).toBe('my value')
   })
 
   test('shallow nested', ()=> {
     let object = {}
-    odotty.set({ object, path: 'a', value: 'my value' })
+    odjectd.set({ object, path: 'a', value: 'my value' })
     expect(object.a).toBe('my value')
   })
 })
@@ -24,29 +24,21 @@ describe('get', () => {
   test('exists', ()=> {
     let object = { a: { b: { c: 'd' } } }
     expect(
-      odotty.get({ object, path: 'a.b.c' })
+      odjectd.get({ object, path: 'a.b.c' })
     ).toBe('d')
   })
 
   test('exist in the middle ', ()=> {
     let object = { a: { b: { c: 'd' } } }
     expect(
-      odotty.get({ object, path: 'a.b' })
+      odjectd.get({ object, path: 'a.b' })
     ).toEqual({ c: 'd' })
   })
 
   test('does not exist', ()=> {
     let object = {}
     expect(
-      odotty.get({ object, path: 'a.b.c' })
+      odjectd.get({ object, path: 'a.b.c' })
     ).toBeUndefined()
   })
-})
-
-test('d', ()=> {
-  const objectd = require('object-dot')
-  let object = { foo: { bar: 'you!' }}
-  console.log(
-    objectd.get({ object, path: 'foo.bar.c.d'})
-  )
 })
